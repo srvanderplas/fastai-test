607e45253a462af47c134b2f50e5db6ae38545a3
Susan Vanderplas <srvanderplas@gmail.com>
2021-10-20 07:37:47 -0500

    first commit

---
+++ basic-image-demo.R
@@ -0,0 +1,38 @@
+
+# reticulate::install_miniconda()
+# reticulate::conda_create('r-reticulate')
+## This creates a r-reticulate conda instance inside /home/susan/.local/share/r-miniconda/envs/r-reticulate
+# devtools::install_github('eagerai/fastai')
+# fastai::install_fastai(gpu = T, cuda_version = '10', overwrite = FALSE)
+
+library(fastai)
+URLs_PETS() # Must restart session for this to work, not sure why?
+path = 'oxford-iiit-pet'
+path_anno = 'oxford-iiit-pet/annotations'
+path_img = 'oxford-iiit-pet/images'
+fnames = get_image_files(path_img)
+
+dls = ImageDataLoaders_from_name_re(
+  path, fnames, pat='(.+)_\\d+.jpg$',
+  item_tfms = RandomResizedCrop(460, min_scale=0.75), bs = 10,
+  batch_tfms = list(aug_transforms(size = 299, max_warp = 0),
+                    Normalize_from_stats( imagenet_stats() )
+  ),
+  device = 'cuda'
+)
+
+dls %>% show_batch()
+
+
+learn = cnn_learner(dls, resnet50(), metrics = error_rate)
+learn %>% fit_one_cycle(n_epoch = 8)
+
+
+learn$unfreeze()
+
+learn %>% fit_one_cycle(3, lr_max = fastai::slice(1e-6,1e-4))
+
+
+interp = ClassificationInterpretation_from_learner(learn)
+interp %>% most_confused()
+
