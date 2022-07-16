f2838f974e1051e420934387e895461358ff3e48
Hiiiua <mhua4816@gmail.com>
2022-07-16 18:33:04 -0500

    Taken care of space in class names

--- Split_bbox.py
+++ Split_bbox.py
@@ -15,10 +15,12 @@ def split_bbox(file_path):
     root = tree.getroot()
     for obj in root.findall('object'):
         names = (obj.find('name').text).split(',')
+        names = sum([i.split(' ') for i in names], []) 
+        names = [i.strip() for i in names if i.strip()]
         rep = len(names) - 1
         while rep > 0:
             dup = copy.deepcopy(obj)
-            dup.find('name').text = names[rep].strip()
+            dup.find('name').text = names[rep]
             root.append(dup)
             rep -= 1
             if rep == 0:
@@ -27,14 +29,7 @@ def split_bbox(file_path):
     t = ET.ElementTree(root)
     with open (file_path, "wb") as file :
         t.write(file)
-    
 
-def all_classes(file_path):
-    f = file_path
-    tree = ET.parse(f)
-    root = tree.getroot()
-    for obj in root.findall('object'):
-        names = (obj.find('name').text).split(',')
         
 paths = get_files('/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation', extensions=['.xml']) # fastai
 class_map = set([])
@@ -43,30 +38,37 @@ for path in paths:
 
 # ------ This part finds out unique classes --------------
     f = path
+    n_file += 1
     tree = ET.parse(f)
     root = tree.getroot()
     for obj in root.findall('object'):
         names = obj.find('name').text
+        if names == None:
+            print(f)
         class_map.add(names)
 print(class_map)
-# Looks more cleaning work than expected
-  # 'qud', 'rectangle', 'hex', 'rounded', 'othre', 'hexagon', 'circle_', 'triangle',
-  # 'etxt', 'stars', 'qudarilateral', 'cirlce', 'quardilateral', 'quadrilateral', 
-  # 'ext', 'cirle', 'quadrilaterl', 'chrevron', 'othere', 'star', 'triangl', 
-  # 'elongated', 'chervron', 'toe', 'circles', 'texture_crepe', 'background', 
-  # 'iother', 'hexagon chevron', 'hatching', None, 'qiad', 'lines', 'quadilateral',
-  # 'quadrilaterals', 'smooth', 'irregular', 'qua', 'ribbon', 'circle text', 'star.',
-  # 'circe', 'quadrliateral', 'ciricle', 'circle triangle', 'quad', 'octagon', 'texture',
-  # 'cricle', 'lkns', 'pentagons', 'crepe', 'texxt', 'chevrons', 'triangles', 
-  # 'texture_smooth', 'bowite', 'blowtie', 'tet', 'oter', 'bowtie', 'circleline',
-  # 'pentagon', 'quarilateral', 'heagon', 'region', 'ttriangle', 'start', 'triangels',
-  # 'circle', 'qaud', 'heel', 'cheron', 'star quad', 'quadrilateralline', 'chrvron',
-  # 'polygon', 'trianlges', 'lie', 'quadrilatteral', 'excludue', 'quadmline',
-  # 'quadrilteral', 'trianglee', 'exclud', 'other', 'quadrilaterlal', 'exclude', 
-  # 'ribon', 'cicle', 'text', 'logo', 'qyad', 'chevron', 'line', 'bootie', 
-  # 'quadcircle', 'curved line'
+# ------ End of finding out unique classes --------------
 
     # split_bbox(path)
 
 t1 = time.time()
 print('The whole splitting process costs', t1-t0, 's')
+
+'qud', 'rectangle', 'hex', 'rounded', 'othre', 'hexagon', 'circle_', 'triangle',
+'curved', 'etxt', 'stars', 'qudarilateral', 'cirlce', 'quardilateral', 
+'quadrilateral', 'ext', 'cirle', 'quadrilaterl', 'chrevron', 'othere', 'star', 
+'triangl', 'elongated', 'chervron', 'toe', 'circles', 'texture_crepe', 
+'background', 'iother', 'hatching', 'qiad', 'lines', 'quadilateral', 
+'quadrilaterals', 'smooth', 'irregular', 'qua', 'ribbon', 'star.', 'circe', 
+'quadrliateral', 'ciricle', 'quad', 'octagon', 'texture', 'cricle', 'lkns', 
+'pentagons', 'crepe', 'texxt', 'chevrons', 'triangles', 'texture_smooth', 
+'bowite', 'blowtie', 'tet', 'oter', 'bowtie', 'circleline', 'pentagon', 
+'quarilateral', 'heagon', 'region', 'ttriangle', 'start', 'star,', 'triangels', 
+'circle', 'qaud', 'heel', 'cheron', 'quadrilateralline', 'chrvron', 'polygon', 
+'trianlges', 'lie', 'quadrilatteral', 'excludue', 'quadmline', 'quadrilteral', 
+'trianglee', 'exclud', 'other', 'quadrilaterlal', 'exclude', 'ribon', 'cicle', 
+'text', 'logo', 'qyad', 'chevron', 'line', 'bootie', 'quadcircle'
+
+
+
+
