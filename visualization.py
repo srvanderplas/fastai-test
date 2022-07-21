a35dfe54a93dca381a88f2761080e0d0844a9912
Hiiiua <mhua4816@gmail.com>
2022-07-21 15:39:44 -0500

    Fixed label offsets

--- visualization.py
+++ visualization.py
@@ -1,78 +1,11 @@
-elongated = ['elongated']
-elongated_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/skechers-performance-go-walk-joy-soothe-burgundy_product_9111058_color_7.xml']
-
-toe = ['toe']
-toe = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/dan-post-thin-blue-line-sand-blue-leather_product_9089659_color_41715.xml']
-
-curved = ['curved']
-curved_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/sorel-kids-cub-toddler-little-kid-big-kid-surplus-green-olive_product_8699081_color_621306.xml',
-                '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/sorel-kids-cub-toddler-little-kid-big-kid-surplus-green-olive_product_8699081_color_621306.xml']
-
-rounded = ['rounded']
-rounded_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/bobs-from-skechers-bobs-squad-glam-le-taupe_product_9065662_color_11.xml']
-
-quadcircle = ['quadcircle']
-quadcircle_fiels = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/skechers-easy-going-tan_product_9089402_color_20.xml']
-
-quadmline = ['quadmline']
-quadmline_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/skechers-performance-go-walk-evolution-ultra-rapids-black_product_9111097_color_3.xml']
-
-circleline = ['circleline']
-circleline_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/merrell-all-out-blaze-sieve-brindle-butterscotch_product_9164043_color_533174.xml',
-                    '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/on-cloud-navy-white_product_8379589_color_513.xml']
-
-quadrilateralline = ['quadrilateralline']
-quadrilateralline_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/jessica-simpson-carnivela-moss-brown-split-suede_product_8974852_color_669353.xml']
-
-lkns = ['lkns']
-lkns_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/ugg-kids-obie-waterproof-toddler-little-kid-norse-green_product_8920281_color_703795.xml']
-
-texture = ['texture']
-texture_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/hunter-kids-original-chelsea-little-kid-big-kid-black_product_8577026_color_3.xml',
-                 '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/onex-ariel-n-white-elastic_product_9035240_color_30091.xml']
-
-region = ['region']
-region_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/new-balance-classics-mz501-red_product_9042319_color_585.xml']
-
-smooth = ['smooth']
-smooth_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/nina-deena-silver-dreamland_product_9107077_color_680591.xml',
-                '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/steve-madden-rachel-platform-sandal-rose-gold_product_9026962_color_35560.xml']
-
-irregular = ['irregular']
-irregular_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/dr-martens-redfin-white-hydro-leather-white-hydro-leather_product_8983105_color_730236.xml',
-                   '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/nike-tanjun-se-team-red-vast-grey-obsidian-navy_product_8709415_color_717098.xml',
-                   '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/spring-step-heroic-black_product_8618717_color_3.xml']
-
-crepe = ['crepe']
-crepe_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/cienta-kids-shoes-76997-toddler-little-kid-big-kid-tiel_product_8108081_color_680257.xml',
-               '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/cienta-kids-shoes-76997-toddler-little-kid-big-kid-tiel_product_8108081_color_680257.xml',
-               '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/cienta-kids-shoes-76997-toddler-little-kid-big-kid-tiel_product_8108081_color_680257.xml',
-               '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/not-rated-xara-cream_product_9042119_color_1892.xml',
-               '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/taos-footwear-carousel-2-blue-embossed-suede_product_8831224_color_638910.xml']
-
-texture_smooth = ['texture_smooth']
-texture_smooth_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/pikolinos-tarifa-06j-0015-cuero_product_8672795_color_23562.xml',
-                        '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/tory-burch-brannan-studded-sandal-leccio_product_9125287_color_717736.xml',
-                        '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/tory-burch-color-block-slip-on-sneaker-perfect-ivory-perfect-black_product_9125293_color_778247.xml']
-
-background = ['background']
-background_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/not-rated-aleksa-peach_product_9042129_color_4912.xml']
-
-texture_crepe = ['texture_crepe']
-texture_crepe_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/wolverine-i-90-epx-carbonmax-wheat_product_8984202_color_716.xml']
-
-heel = ['heel']
-heel_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/dan-post-thin-blue-line-sand-blue-leather_product_9089659_color_41715.xml']
-
-
-hatching = ['hatching']
-hatching_files = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/dingo-adobe-rose-light-brown-distresssed_product_7644821_color_249614.xml',
-                  '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/report-madella-black_product_9035067_color_3.xml',
-                  '/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/roper-american-beauty-fringe-sanded-brown-leather-red-white-fringe_product_9096963_color_769188.xml']
+import cv2
+from random import randint
+from collections import Counter
 
 
 parser = VOCBBoxParser(annotations_dir="Modified Data/Modified Annotation", 
                        images_dir="Modified Data/Images")
+t, v = parser.parse()
 
 def get_bndbox(bbox):
     xmin = int(bbox.xmin)
@@ -83,7 +16,7 @@ def get_bndbox(bbox):
   
 def get_info(record_obj):
   # '''
-  # Returns three lists containing information of bboxes, labels and path, respectively.
+  # Returns three lists containing information of bboxes, labels, path and the non-unique labels, respectively.
   # '''
     all_info = list(record_obj.components)
     str_info = [str(i) for i in all_info]
@@ -96,49 +29,66 @@ def get_info(record_obj):
     labels = all_info[labels_index].labels
     path = all_info[path_index].filepath
     filename = str(path.stem)
+    offset = dict(Counter([get_bndbox(i)[0] for i in bboxes]))
+    offsets = {k:v for k,v in offset.items() if v > 1}
     
-    return bboxes, labels, path, filename
+    return bboxes, labels, path, filename, offsets
     
 def visualize(record_obj):    
     args = {'color': (0, 255, 0),
-            'thickness': 4,
+            'thickness': 2,
             'fontFace': int(cv2.FONT_HERSHEY_SIMPLEX),
             'fontScale': 0.8,
-            'x_offset': 0,
+            'x_offset': 10,
             'y_offset': 20}
         
-    bboxes, labels, path, filename = get_info(record_obj)
+    bboxes, labels, path, filename, offsets = get_info(record_obj)
+    
+    counts = sum([[1] * len(offsets)], [])
+    counts = dict(zip(offsets.keys(), counts))
     
     img = cv2.imread(str(path), cv2.IMREAD_UNCHANGED)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # img = np.zeros((512,512,3), np.uint8)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
     for i in range(len(bboxes)):
+        offset = 1
         xmin, ymin, xmax, ymax = get_bndbox(bboxes[i])
+        if xmin in counts.keys():
+            offset = counts[xmin]
+            counts[xmin] += 1
+            
         cv2.rectangle(img, (xmin, ymin), (xmax, ymax),
             args.get('color'),
             args.get('thickness'))
                   
         cv2.putText(img, 
             labels[i],
-            (xmin + args.get('x_offset'), ymin + args.get('y_offset')),
+            (xmin + args.get('x_offset'), (ymin + offset * args.get('y_offset'))),
             args.get('fontFace'),
             args.get('fontScale'),
             args.get('color'),
             args.get('thickness'))
                 
-    cv2.imshow('VOC', img)
-    # plt.show()
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    cv2.waitKey(1)
+    # cv2.imshow(filename, img) # plotting
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # cv2.waitKey(1)
+    return img
 
-t_paths = hatching_files
-records = train_records+valid_records
+# t_paths = list(set(lkns_files))
+# records = train_records+valid_records
+
+records = t + v
+t_paths = [str(i) for i in paths[:5]]
 
 for t_path in t_paths:
     for i in records:
-        bboxes, labels, path, filename = get_info(i)
+        bboxes, labels, path, filename, offsets = get_info(i)
         if filename in t_path:
             visualize(i)
+            
+            
+for i in [0, 4, 6, 78, 90, 91, 92, 479, 780, 1213]:
+    img = visualize(train_records[i])
+    cv2.imwrite('Vis_{}.png'.format(i), img) # save the image
 
