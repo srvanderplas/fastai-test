a88ba6ab7fad68d5d3c715cd2758c3f36d7d8cf2
Hiiiua <mhua4816@gmail.com>
2022-09-08 14:17:12 -0500

    Dataset

---
+++ FasterRCNN.py
@@ -0,0 +1,104 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+
+classes = ['logo', 'polygon', 'chevron', 'circle', 'text', 'quad', 'other', 'triangle', 'exclude',
+           'star', 'bowtie', 'line', 'ribbon']
+
+# Perform transfer learning with a pretrained faster RCNN model
+base_model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+# Replace the pretrained head
+num_classes = len(classes)
+
+# get number of input features for the classifier
+in_features = base_model.roi_heads.box_predictor.cls_score.in_features
+
+# replace the pre-trained head with a new one
+base_model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+
+# https://www.kaggle.com/code/pestipeti/pytorch-starter-fasterrcnn-train/notebook
+import pandas as pd
+import numpy as np
+import cv2
+import os
+import re
+
+from PIL import Image
+
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+
+import torch
+import torchvision
+
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.sampler import SequentialSampler
+
+from matplotlib import pyplot as plt
+
+import torchvision.datasets as datasets
+
+import xml.etree.ElementTree as ET
+
+dataset = datasets.VOCDetection('/Users/huamuxin/Documents/fastai-test/Modified Data',image_set = 'train', year='2012', transform=False)
+
+class VOC_dataset(Dataset):
+    def __init__(self, dataset_path, transform = None):
+        super().__init__()
+        self.dataset_path = dataset_paths
+        self.image_path = os.path.join(dataset_path, 'JPEGImages')
+        self.target_path = os.path.join(dataset_path, 'Annotations') # XML path
+        assert os.path.exists(self.image_path), 'JPEGImages does not exist'
+        assert os.path.exists(self.image_path), 'Annotations does not exist'
+        
+        self.image_index_list = [s.split('.')[0] for i in os.listdir(self.image_path)]
+        self.target_index_list = [s.split('.')[0] for i in os.listdir(self.target_path)]
+        assert self.image_inded_list == self.target_index_list, 'image names do not match xml names'
+        
+        self.length = len(self.image_index_list)
+        self.transform = transform
+    
+    def simple_parse_xml(self, target_name): # could imporove
+        tree = ET.parse(target_name) # target name is the path of target file
+        root = tree.getroot()
+        labels = [] # list of all labels in current xml file
+        boxes = [] # list of coordinates information in current xml file
+        for obj in find.findall('object'):
+            labels.append(obj.find('name').text)
+            bndbox = obj.find('bndbox')
+            box = [bndbox.find('xmin').text, bndbox.find('ymin').text, bndbox.find('xmax').text, bndbox.find('ymax').text]
+            boxes.append(box)
+        
+        return {'labels' : torch.as_tensor(labels, dtype = torch.int64),
+                'boxes' : torch.as_tensor(boxes, dtype = torch.float)}
+        
+    def __getitem__(self, index):
+        image_name = os.path.join(self.image_path, self.image_index_list[index] + '.jpg')
+        target_name = os.path.join(self.target_path, self.target_index_list[index] + '.xml')
+        
+        image = Image.open(image_name).convert('RGB')
+        target = self.simple_parse_xml(target_name)
+        
+        if self.transform:
+            image = self.transform(image)
+        return {'image' : image, 'target' : target}
+      
+    def __len__(self):
+        return self.length
+    
+    def collate_fn_(self, batch):
+        images = [i['image'] for i in batch]
+        targets = [{'labels' : i['target']['labels'], 'boxes':i['target']['boxes']} for i in batch]
+        
+        return {'images' : images, 'targets' : targets}
+      
+      
