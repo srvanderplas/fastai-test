f67da265d9409922137fe5bd7c2ef62a8c47aaed
Hiiiua <mhua4816@gmail.com>
2022-09-24 17:10:45 -0500

    Complete data split function

--- FasterRCNN.py
+++ FasterRCNN.py
@@ -3,53 +3,70 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-
-
-classes = ['logo', 'polygon', 'chevron', 'circle', 'text', 'quad', 'other', 'triangle', 'exclude',
-           'star', 'bowtie', 'line', 'ribbon']
-
-# Perform transfer learning with a pretrained faster RCNN model
-base_model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-
-# Replace the pretrained head
-num_classes = len(classes)
-
-# get number of input features for the classifier
-in_features = base_model.roi_heads.box_predictor.cls_score.in_features
-
-# replace the pre-trained head with a new one
-base_model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-
-# https://www.kaggle.com/code/pestipeti/pytorch-starter-fasterrcnn-train/notebook
 import pandas as pd
 import numpy as np
 import cv2
 import os
 import re
-
 from PIL import Image
-
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
-
 import torch
 import torchvision
-
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection import FasterRCNN
 from torchvision.models.detection.rpn import AnchorGenerator
-
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.sampler import SequentialSampler
-
 from matplotlib import pyplot as plt
-
 import torchvision.datasets as datasets
 from torchvision import transforms
 from torch.utils.data import DataLoader
 import xml.etree.ElementTree as ET
 
+
+import random
+import os
+    
+def data_split(dataset_path, train_validate_percent = 0.9, train_percent = 0.8, test_percent = 0.1):
+    target_path = os.path.join(dataset_path, 'Modified Annotations')
+    all_xml_file = os.listdir((target_path))
+    
+    num_xml = len(all_xml_file) # total number of xml files
+    num_train_validate = int(num_xml * train_validate_percent)
+    num_test = num_xml - num_train_validate
+    
+    num_train = int(num_train_validate * train_percent)
+    num_validate = num_train_validate - num_train
+    
+    
+    train_validate_idx = random.sample(range(num_xml), num_train_validate)
+    train_idx = random.sample(train_validate_idx, num_train)
+    
+    f_train_val = open(dataset_path + '/train_validate.txt', 'w')
+    f_train = open(dataset_path + '/train.txt', 'w')
+    f_validate = open(dataset_path + '/validate.txt', 'w')
+    f_test = open(dataset_path + '/test.txt', 'w')
+    
+    for i in range(num_xml):
+        name = all_xml_file[i].split('.')[0] + '\n'
+        if i in train_validate_idx:
+            f_train_val.write(name)
+            if i in train_idx:
+                f_train.write(name)
+            else:
+                f_validate.write(name)
+        else:
+              f_test.write(name)
+    
+    f_train_val.close()
+    f_train.close()
+    f_validate.close()
+    f_test.close()
+          
+    
+    
+    
 class VocDataset(Dataset):
     def __init__(self, dataset_path, transform = None):
         super().__init__()
@@ -123,13 +140,90 @@ classes = ['logo', 'polygon', 'chevron', 'circle', 'text', 'quad', 'other', 'tri
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(num_classes=len(classes)+1, pretrained=False, pretrained_backbone = False)
 
 model.train()
-for batch in dl:
+for batch in dl:  # test the S
     pred = model(batch['images'], batch['targets'])
     print(pred)
     break
 
+
+
 '''
 Pytorch Doc at https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
-Will adopt Finetuning from a pretrained model for a quick start, will look at different backbone later
+Kaggle example https://www.kaggle.com/code/pestipeti/pytorch-starter-fasterrcnn-train
 '''
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
+# replace the classifier with a new one, that has
+# num_classes which is user-defined
+num_classes = len(classes) + 1
+# get number of input features for the classifier
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+# replace the pre-trained head with a new one
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+import transforms as T
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.PILToTensor())
+    transforms.append(T.ConvertImageDtype(torch.float))
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+  
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+model.to(device)
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+
+# lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+
+
+for batch in dl:
+    im = batch['images']
+    targ = batch['targets']
+    images = list(image.to(device) for image in im)
+    targets = [{k: v.to(device) for k, v in t.items()} for t in targ]
+    
+    loss_dict = model(images, targets)
+    break
+# {'loss_classifier': tensor(2.5329, grad_fn=<NllLossBackward0>),
+# classifier loss is the loss of prediction of object classes in bounding boxes.
+
+# 'loss_box_reg': tensor(0.0386, grad_fn=<DivBackward0>),
+# Localisation loss in the ROI head. Measures the loss for box localisation (predicted location vs true location).
+
+# 'loss_objectness': tensor(0.4805, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>),
+# This is also when we are extracting the region proposals whether the object is present in the anchorbox or not.
+
+# 'loss_rpn_box_reg': tensor(0.0169, grad_fn=<DivBackward0>)}
+
+num_epochs = 2
+
+itr = 1
+
+for epoch in range(num_epochs):
+    for images, targets, image_ids in dl:
+        
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+        loss_dict = model(images, targets)
+
+        losses = sum(loss for loss in loss_dict.values())
+        loss_value = losses.item()
+
+        loss_hist.send(loss_value)
+
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+
+        if itr % 50 == 0:
+            print(f"Iteration #{itr} loss: {loss_value}")
+
+        itr += 1
+    
+    # update the learning rate
+    if lr_scheduler is not None:
+        lr_scheduler.step()
