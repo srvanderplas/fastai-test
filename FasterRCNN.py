0b10ed166584ba897499abf5d4c77690599e800a
Hiiiua <mhua4816@gmail.com>
2022-09-24 19:32:26 -0500

    Test training on small sample

--- FasterRCNN.py
+++ FasterRCNN.py
@@ -112,8 +112,10 @@ class VocDataset(Dataset):
                 'boxes' : torch.as_tensor(boxes, dtype = torch.float)}
         
     def __getitem__(self, index):
+        # print(index)
         image_name = os.path.join(self.image_path, self.image_index_list[index] + '.jpg')
         target_name = os.path.join(self.target_path, self.target_index_list[index] + '.xml')
+        # print(image_name)
         
         image = Image.open(image_name).convert('RGB')
         target = self.simple_parse_xml(target_name)
@@ -130,6 +132,26 @@ class VocDataset(Dataset):
         targets = [{'labels' : i['target']['labels'], 'boxes':i['target']['boxes']} for i in batch]
         
         return {'images' : images, 'targets' : targets}
+      
+class Averager:
+    def __init__(self):
+        self.current_total = 0.0
+        self.iterations = 0.0
+
+    def send(self, value):
+        self.current_total += value
+        self.iterations += 1
+
+    @property
+    def value(self):
+        if self.iterations == 0:
+            return 0
+        else:
+            return 1.0 * self.current_total / self.iterations
+
+    def reset(self):
+        self.current_total = 0.0
+        self.iterations = 0.0
 
 transform = transforms.Compose(
         [
@@ -141,88 +163,66 @@ transform = transforms.Compose(
 dataset_path = './Modified Data'
 data_split(dataset_path)
 
-ds = VocDataset(dataset_path = dataset_path, transform = transform)
-dl = DataLoader(ds, batch_size = 4, shuffle = True, collate_fn = ds.collate_fn_)
-
-classes = ['logo', 'polygon', 'chevron', 'circle', 'text', 'quad', 'other', 'triangle', 'exclude',
-           'star', 'bowtie', 'line', 'ribbon']
-# num_classes = len(classes)
-# model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False)
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(num_classes=len(classes)+1, pretrained=False, pretrained_backbone = False)
-
-model.train()
-for batch in dl:  # test the S
-    pred = model(batch['images'], batch['targets'])
-    print(pred)
-    break
-
-
-
+--------
 '''
 Pytorch Doc at https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
 Kaggle example https://www.kaggle.com/code/pestipeti/pytorch-starter-fasterrcnn-train
 '''
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
-# replace the classifier with a new one, that has
-# num_classes which is user-defined
-num_classes = len(classes) + 1
-# get number of input features for the classifier
-in_features = model.roi_heads.box_predictor.cls_score.in_features
-# replace the pre-trained head with a new one
-model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+train_ds  = VocDataset(dataset_path = dataset_path, transform = transform, mode = 'train')
+valid_ds  = VocDataset(dataset_path = dataset_path, transform = transform, mode = 'validate')
 
-import transforms as T
+classes = ['logo', 'polygon', 'chevron', 'circle', 'text', 'quad', 'other', 'triangle', 'exclude',
+           'star', 'bowtie', 'line', 'ribbon']
+           
 
-def get_transform(train):
-    transforms = []
-    transforms.append(T.PILToTensor())
-    transforms.append(T.ConvertImageDtype(torch.float))
-    if train:
-        transforms.append(T.RandomHorizontalFlip(0.5))
-    return T.Compose(transforms)
-  
+train_dl = DataLoader(train_ds, batch_size = 4, shuffle = True, collate_fn = ds.collate_fn_)
+valid_dl = DataLoader(valid_ds, batch_size = 4, shuffle = True, collate_fn = ds.collate_fn_)
+
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 model.to(device)
+
 params = [p for p in model.parameters() if p.requires_grad]
 optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
-
 # lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
-
-
-for batch in dl:
-    im = batch['images']
-    targ = batch['targets']
-    images = list(image.to(device) for image in im)
-    targets = [{k: v.to(device) for k, v in t.items()} for t in targ]
-    
-    loss_dict = model(images, targets)
-    break
-# {'loss_classifier': tensor(2.5329, grad_fn=<NllLossBackward0>),
-# classifier loss is the loss of prediction of object classes in bounding boxes.
-
-# 'loss_box_reg': tensor(0.0386, grad_fn=<DivBackward0>),
-# Localisation loss in the ROI head. Measures the loss for box localisation (predicted location vs true location).
-
-# 'loss_objectness': tensor(0.4805, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>),
-# This is also when we are extracting the region proposals whether the object is present in the anchorbox or not.
-
-# 'loss_rpn_box_reg': tensor(0.0169, grad_fn=<DivBackward0>)}
+lr_scheduler = None
 
 num_epochs = 2
-
 itr = 1
 
-for epoch in range(num_epochs):
-    for images, targets, image_ids in dl:
-        
-        images = list(image.to(device) for image in images)
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+loss_hist = Averager()
 
+t_ds = VocDataset(dataset_path = dataset_path, transform = transform, mode = 'train')
+t_dl = DataLoader(t_ds, batch_size = 4, shuffle = True, collate_fn = ds.collate_fn_)
+
+for epoch in range(num_epochs):
+    for batch in t_dl:
+        
+        loss_hist.reset()
+        # images = list(image.to(device) for image in images)
+        # targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+        
+        im = batch['images']
+        targ = batch['targets']
+        images = list(image.to(device) for image in im)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targ]
+    
         loss_dict = model(images, targets)
+      
+        # break
+        print('loss_dict: ', loss_dict)
+        # {'loss_classifier': tensor(6.3938, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1693, grad_fn=<DivBackward0>), 
+        # 'loss_objectness': tensor(1.6913, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 
+        # 'loss_rpn_box_reg': tensor(0.9039, grad_fn=<DivBackward0>)}
 
         losses = sum(loss for loss in loss_dict.values())
         loss_value = losses.item()
+
+        print('losses: ', losses)
+        # tensor(9.1583, grad_fn=<AddBackward0>)
+
+        print('loss_value: ', loss_value)
+        # 9.158288955688477
 
         loss_hist.send(loss_value)
 
@@ -230,11 +230,125 @@ for epoch in range(num_epochs):
         losses.backward()
         optimizer.step()
 
-        if itr % 50 == 0:
+        if itr % 1 == 0: # 50
             print(f"Iteration #{itr} loss: {loss_value}")
 
         itr += 1
-    
+        
+# loss_dict:  {'loss_classifier': tensor(6.3938, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1693, grad_fn=<DivBackward0>), 'loss_objectness': tensor(1.6913, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.9039, grad_fn=<DivBackward0>)}        
+# losses:  tensor(9.1583, grad_fn=<AddBackward0>)        
+# loss_value:  9.158288955688477        
+# Iteration #2 loss: 9.158288955688477        
+# loss_dict:  {'loss_classifier': tensor(6.2422, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.0992, grad_fn=<DivBackward0>), 'loss_objectness': tensor(1.6525, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.6531, grad_fn=<DivBackward0>)}
+# losses:  tensor(8.6470, grad_fn=<AddBackward0>)
+# loss_value:  8.646978378295898
+# Iteration #3 loss: 8.646978378295898
+# loss_dict:  {'loss_classifier': tensor(6.3935, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.2516, grad_fn=<DivBackward0>), 'loss_objectness': tensor(1.7266, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.7815, grad_fn=<DivBackward0>)}
+# losses:  tensor(9.1533, grad_fn=<AddBackward0>)
+# loss_value:  9.153275489807129
+# Iteration #4 loss: 9.153275489807129
+# loss_dict:  {'loss_classifier': tensor(6.0925, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.2861, grad_fn=<DivBackward0>), 'loss_objectness': tensor(1.7179, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.9329, grad_fn=<DivBackward0>)}
+# losses:  tensor(9.0295, grad_fn=<AddBackward0>)
+# loss_value:  9.029480934143066
+# Iteration #5 loss: 9.029480934143066        
+# loss_dict:  {'loss_classifier': tensor(5.7527, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.3174, grad_fn=<DivBackward0>), 'loss_objectness': tensor(1.8469, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.6728, grad_fn=<DivBackward0>)}
+# losses:  tensor(8.5898, grad_fn=<AddBackward0>)
+# loss_value:  8.589811325073242
+# Iteration #6 loss: 8.589811325073242   
+# loss_dict:  {'loss_classifier': tensor(6.3617, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.2610, grad_fn=<DivBackward0>), 'loss_objectness': tensor(1.6953, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.9091, grad_fn=<DivBackward0>)}
+# losses:  tensor(9.2271, grad_fn=<AddBackward0>)
+# loss_value:  9.227115631103516
+# Iteration #7 loss: 9.227115631103516        
+# loss_dict:  {'loss_classifier': tensor(6.3778, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.2174, grad_fn=<DivBackward0>), 'loss_objectness': tensor(1.6300, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.9175, grad_fn=<DivBackward0>)}
+# losses:  tensor(9.1427, grad_fn=<AddBackward0>)
+# loss_value:  9.142691612243652
+# Iteration #8 loss: 9.142691612243652        
+# loss_dict:  {'loss_classifier': tensor(6.3670, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1742, grad_fn=<DivBackward0>), 'loss_objectness': tensor(1.6477, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.4726, grad_fn=<DivBackward0>)}
+# losses:  tensor(8.6615, grad_fn=<AddBackward0>)
+# loss_value:  8.66147232055664
+# Iteration #9 loss: 8.66147232055664
+# loss_dict:  {'loss_classifier': tensor(6.2625, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.3150, grad_fn=<DivBackward0>), 'loss_objectness': tensor(1.6337, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.7637, grad_fn=<DivBackward0>)}
+# losses:  tensor(8.9750, grad_fn=<AddBackward0>)
+# loss_value:  8.974950790405273
+# Iteration #10 loss: 8.974950790405273
+
     # update the learning rate
     if lr_scheduler is not None:
         lr_scheduler.step()
+    print(f"Epoch #{epoch} loss: {loss_hist.value}")
+------
+
+# 
+# ds = VocDataset(dataset_path = dataset_path, transform = transform)
+# dl = DataLoader(ds, batch_size = 4, shuffle = True, collate_fn = ds.collate_fn_)
+# 
+# classes = ['logo', 'polygon', 'chevron', 'circle', 'text', 'quad', 'other', 'triangle', 'exclude',
+#            'star', 'bowtie', 'line', 'ribbon']
+# num_classes = len(classes)
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False)
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(num_classes=len(classes)+1, pretrained=False, pretrained_backbone = False)
+
+model.train()
+for batch in t_dl:  # test the S
+    pred = model(batch['images'], batch['targets'])
+    print(pred)
+    break
+
+# {'loss_classifier': tensor(6.4603, grad_fn=<NllLossBackward0>), 'loss_box_reg': tensor(0.1781, grad_fn=<DivBackward0>),
+# 'loss_objectness': tensor(1.5828, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>), 'loss_rpn_box_reg': tensor(0.8501, 
+# grad_fn=<DivBackward0>)}
+
+-----
+# model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+# 
+# # replace the classifier with a new one, that has
+# # num_classes which is user-defined
+# num_classes = len(classes) + 1
+# # get number of input features for the classifier
+# in_features = model.roi_heads.box_predictor.cls_score.in_features
+# # replace the pre-trained head with a new one
+# model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+# 
+# import transforms as T
+# 
+# def get_transform(train):
+#     transforms = []
+#     transforms.append(T.PILToTensor())
+#     transforms.append(T.ConvertImageDtype(torch.float))
+#     if train:
+#         transforms.append(T.RandomHorizontalFlip(0.5))
+#     return T.Compose(transforms)
+#   
+# device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+# model.to(device)
+# params = [p for p in model.parameters() if p.requires_grad]
+# optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+# 
+# # lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+# 
+# 
+# for batch in dl:
+#     im = batch['images']
+#     targ = batch['targets']
+#     images = list(image.to(device) for image in im)
+#     targets = [{k: v.to(device) for k, v in t.items()} for t in targ]
+#     
+#     loss_dict = model(images, targets)
+#     break
+# # {'loss_classifier': tensor(2.5329, grad_fn=<NllLossBackward0>),
+# # classifier loss is the loss of prediction of object classes in bounding boxes.
+# 
+# # 'loss_box_reg': tensor(0.0386, grad_fn=<DivBackward0>),
+# # Localisation loss in the ROI head. Measures the loss for box localisation (predicted location vs true location).
+# 
+# # 'loss_objectness': tensor(0.4805, grad_fn=<BinaryCrossEntropyWithLogitsBackward0>),
+# # This is also when we are extracting the region proposals whether the object is present in the anchorbox or not.
+# 
+# # 'loss_rpn_box_reg': tensor(0.0169, grad_fn=<DivBackward0>)}
+# 
+# num_epochs = 2
+# 
+# itr = 1
+
+
+
