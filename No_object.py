c59ff53ccbedb1d99cd8c9d023176595ae7a4954
Hiiiua <mhua4816@gmail.com>
2022-10-10 10:12:32 -0500

    code for removing no object data

--- No_object.py
+++ No_object.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import glob
+import os
 
 no_obj_list = []
 
@@ -19,3 +20,21 @@ for file_path in file_paths:
     root = tree.getroot()
     if len(root.findall('object')) == 0:
         no_obj_list.append(file_path)
+        
+no_obj = [i.split('/')[-1] for i in no_obj_list]
+no_obj = [i.split('.')[0] + '\n' for i in no_obj]
+
+
+f_train = open('/Users/huamuxin/Documents/fastai-test/Modified Data/train.txt', 'r')
+train_file = f_train.readlines()
+f_train.close()
+
+for delete in no_obj:
+    for train in train_file:
+        if train == delete:
+            train_file.remove(train)
+
+f_train2 = open(dataset_path + '/train2.txt', 'w')
+for each in train_file:
+    f_train2.write(each)
+f_train2.close()
