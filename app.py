2147d244419c183588a513635829e0dd7b611a52
Hiiiua <mhua4816@gmail.com>
2022-11-22 17:12:41 -0600

    origin plot completed

--- app.py
+++ app.py
@@ -5,24 +5,10 @@ import numpy as np
 import random
 import torch
 import cv2
+import shiny_vis
 
-classes = ['logo', 'polygon', 'chevron', 'circle', 'text', 'quad', 'other', 
-           'triangle', 'exclude', 'star', 'bowtie', 'line', 'ribbon']
            
 app_ui = ui.page_fluid(
-    # ui.input_checkbox("logo", "logo"),
-    # ui.input_checkbox("polygon", "polygon"),
-    # ui.input_checkbox("chevron", "chevron"),
-    # ui.input_checkbox("circle", "circle"),
-    # ui.input_checkbox("text", "text"),
-    # ui.input_checkbox("quad", "quad"),
-    # ui.input_checkbox("other", "other"),
-    # ui.input_checkbox("triangle", "triangle"),
-    # ui.input_checkbox("exclude", "exclude"),
-    # ui.input_checkbox("star", "star"),
-    # ui.input_checkbox("bowtie", "bowtie"),
-    # ui.input_checkbox("line", "line"),
-    # ui.input_checkbox("ribbon", "ribbon"),
     ui.input_slider("n_box", "Number of Boxes", value=1, min=0, max=20),
     ui.input_checkbox("randomness", "Random generate"),
     ui.input_slider("idx", "Number between 0 and 897", value=23, min=0, max=897),
@@ -34,7 +20,7 @@ app_ui = ui.page_fluid(
 def server(input, output, session):
     @output
     @render.text
-    def input_index():
+    def classes():
         return f"The value of randomess is {input.randomness()}"
     
     @output
@@ -44,29 +30,87 @@ def server(input, output, session):
             idx = str(random.randint(0, 897))
         if input.randomness() == False:
             idx = input.idx()
-
+        
         origin_name = 'Modified Data/Valid_pred/origin' + str(idx) + '.pt'
         batch = torch.load(origin_name)
 
         tensor_img = batch['images'][0]
         img = tensor_img.permute(1,2,0).cpu().numpy()
+        img_copy = img.copy()
+        
+        classes = ['logo', 'polygon', 'chevron', 'circle', 'text', 'quad', 'other', 
+           'triangle', 'exclude', 'star', 'bowtie', 'line', 'ribbon']
+        labels = [classes[i] for i in batch['targets'][0]['labels']]
 
         tensor_boxes = batch['targets'][0]['boxes']
         np_boxes = tensor_boxes.cpu().detach().numpy().astype(np.int32)
-
+        l_bboxes = np_boxes.tolist()
+        bboxes = np.column_stack((np_boxes, labels))
+        [l_bboxes[i].append(labels[i]) for i in range(len(labels))]
+        
+        label_color=(255,0,255)
+        corner_color = (0,255,255)
+        box_color=(220, 0, 0)
+        length=20
+        
         fig, ax = plt.subplots(1, 1, figsize=(16, 8))
-        img_copy = img.copy()
-        for box in np_boxes:
-            cv2.rectangle(img_copy,
-                          (box[0], box[1]),
-                          (box[2], box[3]),
-                          (220, 0, 0), 3)
-
+        draw_bboxes_and_label(l_bboxes, img_copy)
         ax.set_axis_off()
         ax.imshow(img_copy)
         # plt.show()
         plt.close()
         return fig
+        
+    def draw_bboxes_and_label(l_bboxes, img_copy,label_color=(255,0,255), corner_color = (0,255,255), box_color=(220, 0, 0), length=20):
+        for bbox in l_bboxes:
+            label = bbox[-1]
+            label_size = cv2.getTextSize(label + '0' , cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0] # cv2.getTextSize('Test', font, fontScale, thickness) for width and height
+            cv2.rectangle(img_copy,
+                              (bbox[0], bbox[1]),
+                              (bbox[2], bbox[3]),
+                              box_color, 3)
+            if bbox[1] - label_size[1] - 3 < 0:
+                cv2.rectangle(img_copy,
+                              (bbox[0], bbox[1] + 2),
+                              (bbox[0] + label_size[0], bbox[1] + label_size[1] + 3),
+                              color=label_color,
+                              thickness=-1
+                              )
+                cv2.putText(img_copy, label,
+                            (bbox[0], bbox[1] + label_size + 3),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1,
+                            (0, 0, 0),
+                            thickness=2
+                            )
+            else:
+                cv2.rectangle(img_copy,
+                              (bbox[0] , bbox[1] - label_size[1] - 3),
+                              (bbox[0] + label_size[0], bbox[1] - 3),
+                              color=label_color,
+                              thickness=-1 
+                              )
+                cv2.putText(img_copy, label,
+                            (bbox[0], bbox[1] - 3),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1,
+                            (0, 0, 0),
+                            thickness=2
+                            )
+            
+        cv2.line(img_copy, (bbox[0], bbox[1]), (bbox[0] + length, bbox[1]), corner_color, thickness=3)
+        cv2.line(img_copy, (bbox[0], bbox[1]), (bbox[0], bbox[1] + length), corner_color, thickness=3)
+        # Top Right
+        cv2.line(img_copy, (bbox[2], bbox[1]), (bbox[2] - length, bbox[1]), corner_color, thickness=3)
+        cv2.line(img_copy, (bbox[2], bbox[1]), (bbox[2], bbox[1] + length), corner_color, thickness=3)
+        # Bottom Left
+        cv2.line(img_copy, (bbox[0], bbox[3]), (bbox[0] + length, bbox[3]), corner_color, thickness=3)
+        cv2.line(img_copy, (bbox[0], bbox[3]), (bbox[0], bbox[3] - length), corner_color, thickness=3)
+        # Bottom Right
+        cv2.line(img_copy, (bbox[2], bbox[3]), (bbox[2] - length, bbox[3]), corner_color, thickness=3)
+        cv2.line(img_copy, (bbox[2], bbox[3]), (bbox[2], bbox[3] - length), corner_color, thickness=3)
+        
+        
 
 
 app = App(app_ui, server)
