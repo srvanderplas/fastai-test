802f33d2013292a30406728247dc98b6f2eaf8d6
Hiiiua <mhua4816@gmail.com>
2022-11-22 20:47:04 -0600

    Few changes

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app_ui = ui.page_fluid(
       
         ui.panel_sidebar(
             ui.input_slider("n_boxes", "Number of Boxes", value=1, min=0, max=20),
-            ui.input_checkbox("randomness", "Random generate"),
+            # ui.input_checkbox("randomness", "Random generate"),
             ui.input_numeric("idx", "Number between 0 and 897", value=10),
             ui.output_table("origin_and_pred_classes"),
             ui.output_text("threshold"),
@@ -34,10 +34,8 @@ def server(input, output, session):
     @output
     @render.text
     def threshold():
-        if input.randomness() == False:
-            pred_name = names(input.randomness())[1]
-            pred_batch = torch.load(pred_name)
-            threshold = format(pred_batch[0]['scores'][input.n_boxes()], '.3f')
+        pred_batch = batches()[1]
+        threshold = format(pred_batch[0]['scores'][input.n_boxes()], '.3f')
         return f'The threshold is {threshold}'
       
     @output
@@ -50,13 +48,13 @@ def server(input, output, session):
         df_all['pred_freq'] = 0
         df_all.loc['Total'] = [0, 'Total', input.n_boxes()]
         
-        origin_name = names(input.randomness())[0]
-        pred_name = names(input.randomness())[1]
+        origin_batch, pred_batch = batches()
         
-        origin_batch = torch.load(origin_name)
+        # pred_name = names(input.randomness())[1]
+        
+        # origin_batch = torch.load(origin_name)
         origin_labels = pd.DataFrame([classes[i] for i in origin_batch['targets'][0]['labels']])
-        
-        pred_batch = torch.load(pred_name)
+        # pred_batch = torch.load(pred_name)
         pred_labels = pd.DataFrame([classes[i] for i in pred_batch[0]['labels']])[:input.n_boxes()]
         
         origin_freq = origin_labels.value_counts()
@@ -71,23 +69,25 @@ def server(input, output, session):
         
         return df_all
         
-    def names(random):
-        if random == True:
-            idx = str(random.randint(0, 897))
-        if random == False:
-            idx = input.idx()
+    def batches():
+        # if random_n == True:
+        # idx = random.randint(0, 896)
+        # if random_n == False:
+        idx = input.idx()
         
         origin_name = 'Modified Data/Valid_pred/origin' + str(idx) + '.pt'
         pred_name = 'Modified Data/Valid_pred/pred' + str(idx) + '.pt'
-        return [origin_name, pred_name]
+        
+        origin_batch = torch.load(origin_name)
+        pred_batch = torch.load(pred_name)
+        
+        return [origin_batch, pred_batch]
     
     @output
     @render.plot
     def pred():
-        origin_name, pred_name = names(input.randomness())
-        origin_batch = torch.load(origin_name)
-        pred_batch = torch.load(pred_name)
-        
+        origin_batch, pred_batch = batches()
+
         tensor_img = origin_batch['images'][0]
         img = tensor_img.permute(1,2,0).cpu().numpy()
         img_copy = img.copy()
@@ -122,17 +122,17 @@ def server(input, output, session):
         # if input.randomness() == False:
         #     idx = input.idx()
         
-        origin_name = names(input.randomness())[0]
+        # origin_name = names(input.randomness())[0]
         # origin_name = 'Modified Data/Valid_pred/origin' + str(idx) + '.pt'
-        batch = torch.load(origin_name)
+        origin_batch = batches()[0]
 
-        tensor_img = batch['images'][0]
+        tensor_img = origin_batch['images'][0]
         img = tensor_img.permute(1,2,0).cpu().numpy()
         img_copy = img.copy()
         
-        labels = [classes[i] for i in batch['targets'][0]['labels']]
+        labels = [classes[i] for i in origin_batch['targets'][0]['labels']]
 
-        tensor_boxes = batch['targets'][0]['boxes']
+        tensor_boxes = origin_batch['targets'][0]['boxes']
         np_boxes = tensor_boxes.cpu().detach().numpy().astype(np.int32)
         l_bboxes = np_boxes.tolist()
         bboxes = np.column_stack((np_boxes, labels))
@@ -193,17 +193,17 @@ def server(input, output, session):
                             thickness=2
                             )
             
-        cv2.line(img_copy, (bbox[0], bbox[1]), (bbox[0] + length, bbox[1]), corner_color, thickness=3)
-        cv2.line(img_copy, (bbox[0], bbox[1]), (bbox[0], bbox[1] + length), corner_color, thickness=3)
-        # Top Right
-        cv2.line(img_copy, (bbox[2], bbox[1]), (bbox[2] - length, bbox[1]), corner_color, thickness=3)
-        cv2.line(img_copy, (bbox[2], bbox[1]), (bbox[2], bbox[1] + length), corner_color, thickness=3)
-        # Bottom Left
-        cv2.line(img_copy, (bbox[0], bbox[3]), (bbox[0] + length, bbox[3]), corner_color, thickness=3)
-        cv2.line(img_copy, (bbox[0], bbox[3]), (bbox[0], bbox[3] - length), corner_color, thickness=3)
-        # Bottom Right
-        cv2.line(img_copy, (bbox[2], bbox[3]), (bbox[2] - length, bbox[3]), corner_color, thickness=3)
-        cv2.line(img_copy, (bbox[2], bbox[3]), (bbox[2], bbox[3] - length), corner_color, thickness=3)
+            cv2.line(img_copy, (bbox[0], bbox[1]), (bbox[0] + length, bbox[1]), corner_color, thickness=3)
+            cv2.line(img_copy, (bbox[0], bbox[1]), (bbox[0], bbox[1] + length), corner_color, thickness=3)
+            # Top Right
+            cv2.line(img_copy, (bbox[2], bbox[1]), (bbox[2] - length, bbox[1]), corner_color, thickness=3)
+            cv2.line(img_copy, (bbox[2], bbox[1]), (bbox[2], bbox[1] + length), corner_color, thickness=3)
+            # Bottom Left
+            cv2.line(img_copy, (bbox[0], bbox[3]), (bbox[0] + length, bbox[3]), corner_color, thickness=3)
+            cv2.line(img_copy, (bbox[0], bbox[3]), (bbox[0], bbox[3] - length), corner_color, thickness=3)
+            # Bottom Right
+            cv2.line(img_copy, (bbox[2], bbox[3]), (bbox[2] - length, bbox[3]), corner_color, thickness=3)
+            cv2.line(img_copy, (bbox[2], bbox[3]), (bbox[2], bbox[3] - length), corner_color, thickness=3)
         
         
     
