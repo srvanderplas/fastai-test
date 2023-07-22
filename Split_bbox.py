75a3499d5899810f355a5f707e36a0d7c9cf6063
Hiiiua <mhua4816@gmail.com>
2023-07-22 13:05:17 -0500

    refactoring

--- Split_bbox.py
+++ Split_bbox.py
@@ -6,222 +6,88 @@ import utils
 from PIL import Image
 import pandas as pd
 import numpy as np
+from type_classes import *
+# import typo_classes
 
 t0 = time.time()
 
+def get_num_rep(obj):
+    '''
+    This function gets how many replicated boxes should have
+    '''
+    names = (objec.find('name').text).split(',')
+    names = sum([i.split(' ') for i in names], []) 
+    names = [i.strip() for i in names if i.strip()]
+    num_rep = len(names) - 1
+    return num_rep
+
+def make_rep(num_rep):
+    '''This fuction replicates the boxes'''
+    while num_rep > 0:
+            dup = copy.deepcopy(obj)
+            dup.find('name').text = names[rep]
+            root.append(dup)
+            num_rep -= 1
+            if num_rep == 0:
+                obj.find('name').text = names[0]
+    
 def split_bbox(file_path):
+    '''This fuction rewrites the xml files that have replicated annotation by
+       replicating the boxes with individual class'''
     f = file_path
     tree = ET.parse(f)
     root = tree.getroot()
     for obj in root.findall('object'):
-        names = (obj.find('name').text).split(',')
-        names = sum([i.split(' ') for i in names], []) 
-        names = [i.strip() for i in names if i.strip()]
-        rep = len(names) - 1
-        while rep > 0:
-            dup = copy.deepcopy(obj)
-            dup.find('name').text = names[rep]
-            root.append(dup)
-            rep -= 1
-            if rep == 0:
-                obj.find('name').text = names[0]
+        n_rep = get_num_rep(obj)
+        make_rep(n_rep)
     
     t = ET.ElementTree(root)
     with open (file_path, "wb") as file :
         t.write(file)
 
-        
-paths = get_files('/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation', extensions=['.xml']) # fastai
-class_map = set([])
-for path in paths:
-    path=str(path)
-
-# ------ This part finds out unique classes --------------
+def find_unique_classes(path):
+    unique_class = set([])
     f = path
     tree = ET.parse(f)
     root = tree.getroot()
     for obj in root.findall('object'):
         names = obj.find('name').text
         class_map.add(names)
-print(class_map)
-# ------ End of finding out unique classes --------------
-
-    # split_bbox(path) # split bbox
-
-t1 = time.time()
-print('The whole splitting process costs', t1-t0, 's')
-
-
-cm = ['qud', 'rectangle', 'hex', 'rounded', 'othre', 'hexagon', 'circle_', 'triangle',
-'curved', 'etxt', 'stars', 'qudarilateral', 'cirlce', 'quardilateral', 
-'quadrilateral', 'ext', 'cirle', 'quadrilaterl', 'chrevron', 'othere', 'star', 
-'triangl', 'elongated', 'chervron', 'toe', 'circles', 'texture_crepe', 
-'background', 'iother', 'hatching', 'qiad', 'lines', 'quadilateral', 
-'quadrilaterals', 'smooth', 'irregular', 'qua', 'ribbon', 'star.', 'circe', 
-'quadrliateral', 'ciricle', 'quad', 'octagon', 'texture', 'cricle', 'lkns', 
-'pentagons', 'crepe', 'texxt', 'chevrons', 'triangles', 'texture_smooth', 
-'bowite', 'blowtie', 'tet', 'oter', 'bowtie', 'circleline', 'pentagon', 
-'quarilateral', 'heagon', 'region', 'ttriangle', 'start', 'star,', 'triangels', 
-'circle', 'qaud', 'heel', 'cheron', 'quadrilateralline', 'chrvron', 'polygon', 
-'trianlges', 'lie', 'quadrilatteral', 'excludue', 'quadmline', 'quadrilteral', 
-'trianglee', 'exclud', 'other', 'quadrilaterlal', 'exclude', 'ribon', 'cicle', 
-'text', 'logo', 'qyad', 'chevron', 'line', 'bootie', 'quadcircle']
-
-
-
-
-logo = ['logo']
-
-polygon = ['polygon']
-
-octagon = ['octagon'] # 21
-
-pentagon = ['pentagon', 'pentagons']
-
-hexagon = ['hexagon', 'heagon', 'hex'] # 1133
-
-chevron = ['chevron', 'chrevron', 'chervron', 'chrvron', 'chevrons', 'cheron', ]
-
-circle = ['circle', 'circle_', 'cirlce', 'cirle', 'circles', 'circe', 'ciricle',
-          'cricle', 'cicle']
-          
-text = ['text', 'etxt', 'ext', 'tet', 'texxt']
-
-quad = ['quad', 'qud', 'qiad', 'qua', 'qyad', 'qaud',
-        'quadrilatteral', 'quadrliateral', 'quadrilaterals',
-        'quardilateral', 'quadrilaterlal','quarilateral', 
-        'quadrilaterl', 'quadilateral', 'qudarilateral', 'quadrilteral', 
-        'rectangle', 'quadrilateral']
+    return unique_class
         
-other = ['other', 'othre', 'othere', 'iother', 'oter']
 
-triangle = ['triangle', 'triangl', 'triangles', 'triangels', 'trianglee',
-            'trianlges', 'ttriangle']
+if __name__ == '__main__':
+    paths = get_files('/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation', extensions=['.xml']) # fastai
+    class_map = set([])
+    for path in paths:
+        path=str(path)
+        # find_unique_classes(path) # to find all classes annotated
+        # split_bbox(path) # to split bbox 
+    
+    
+    
+    t1 = time.time()
+    print('The whole splitting process costs', t1-t0, 's')
+    
+    # paths = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/ys-by-yohji-yamamoto-regular-sneaker-off-white_product_9109337_color_527.xml']         
+    for path in paths:
+        path=str(path)
+        tree = ET.parse(path)
+        root = tree.getroot()
+        for obj in root.findall('object'):
+            idx = 0
+            name = obj.find('name').text
+            # for cls in classes:
+            #     idx = name in cls
+            #     if idx:
+            #         count[str(cls[0])] += 1
+            # 
+            if name == 'elongated':
+                print(path)
+    print('count')
             
-exclude = ['exclude', 'exclud', 'excludue']
-
-star = ['star', 'start', 'star.', 'star,','stars']
-
-bowtie = ['bowtie', 'bootie', 'blowtie', 'bowite']
-
-line = ['line', 'lie', 'lines']
-
-ribbon = ['ribbon', 'ribon']
-
-elongated = ['elongated']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/skechers-performance-go-walk-joy-soothe-burgundy_product_9111058_color_7.xml
-
-toe = ['toe']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/dan-post-thin-blue-line-sand-blue-leather_product_9089659_color_41715.xml
-
-curved = ['curved']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/sorel-kids-cub-toddler-little-kid-big-kid-surplus-green-olive_product_8699081_color_621306.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/sorel-kids-cub-toddler-little-kid-big-kid-surplus-green-olive_product_8699081_color_621306.xml
-
-rounded = ['rounded']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/bobs-from-skechers-bobs-squad-glam-le-taupe_product_9065662_color_11.xml
-
-quadcircle = ['quadcircle']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/skechers-easy-going-tan_product_9089402_color_20.xml
-
-quadmline = ['quadmline']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/skechers-performance-go-walk-evolution-ultra-rapids-black_product_9111097_color_3.xml
-
-circleline = ['circleline']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/merrell-all-out-blaze-sieve-brindle-butterscotch_product_9164043_color_533174.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/on-cloud-navy-white_product_8379589_color_513.xml
-
-quadrilateralline = ['quadrilateralline']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/jessica-simpson-carnivela-moss-brown-split-suede_product_8974852_color_669353.xml
-
-lkns = ['lkns']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/ugg-kids-obie-waterproof-toddler-little-kid-norse-green_product_8920281_color_703795.xml
-
-texture = ['texture']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/hunter-kids-original-chelsea-little-kid-big-kid-black_product_8577026_color_3.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/onex-ariel-n-white-elastic_product_9035240_color_30091.xml
-
-region = ['region']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/new-balance-classics-mz501-red_product_9042319_color_585.xml
-
-smooth = ['smooth']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/nina-deena-silver-dreamland_product_9107077_color_680591.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/steve-madden-rachel-platform-sandal-rose-gold_product_9026962_color_35560.xml
-
-irregular = ['irregular']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/dr-martens-redfin-white-hydro-leather-white-hydro-leather_product_8983105_color_730236.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/nike-tanjun-se-team-red-vast-grey-obsidian-navy_product_8709415_color_717098.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/spring-step-heroic-black_product_8618717_color_3.xml
-
-crepe = ['crepe']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/cienta-kids-shoes-76997-toddler-little-kid-big-kid-tiel_product_8108081_color_680257.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/cienta-kids-shoes-76997-toddler-little-kid-big-kid-tiel_product_8108081_color_680257.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/cienta-kids-shoes-76997-toddler-little-kid-big-kid-tiel_product_8108081_color_680257.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/not-rated-xara-cream_product_9042119_color_1892.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/taos-footwear-carousel-2-blue-embossed-suede_product_8831224_color_638910.xml
-
-texture_smooth = ['texture_smooth']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/pikolinos-tarifa-06j-0015-cuero_product_8672795_color_23562.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/tory-burch-brannan-studded-sandal-leccio_product_9125287_color_717736.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/tory-burch-color-block-slip-on-sneaker-perfect-ivory-perfect-black_product_9125293_color_778247.xml
-
-background = ['background']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/not-rated-aleksa-peach_product_9042129_color_4912.xml
-
-texture_crepe = ['texture_crepe']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/wolverine-i-90-epx-carbonmax-wheat_product_8984202_color_716.xml
-heel = ['heel']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/dan-post-thin-blue-line-sand-blue-leather_product_9089659_color_41715.xml
-hatching = ['hatching']
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/dingo-adobe-rose-light-brown-distresssed_product_7644821_color_249614.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/report-madella-black_product_9035067_color_3.xml
-# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/roper-american-beauty-fringe-sanded-brown-leather-red-white-fringe_product_9096963_color_769188.xml
-
-paths = get_files('/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation', extensions=['.xml']) # fastai
-class_map = set([])
-classes = [logo, polygon, octagon, pentagon, hexagon, chevron,
-           circle, text, quad, other, triangle, exclude, star,
-           bowtie, line, ribbon]
-count = {'logo': 0, 'polygon': 0, 'octagon': 0, 'pentagon': 0, 'hexagon': 0, 
-         'chevron': 0, 'circle': 0, 'text': 0, 'quad': 0, 'other': 0, 
-         'triangle': 0, 'exclude': 0, 'star': 0, 'bowtie': 0, 'line': 0, 
-         'ribbon': 0}
-# count_1 = {'logo': [logo:0],
-#            'polygon': [polygon: 0],
-#            'octagon': [octagon: 0],
-#            'pentagon': [pentagon: 0], 
-#            'hexagon': [hexagon: 0], 
-#            'chevron': [chevron: 0], 
-#            'circle': [circle: 0], 
-#            'text': [text: 0], 
-#            'quad': [quad: 0], 
-#            'other': [other: 0], 
-#            'triangle': [triangle: 0], 
-#            'exclude': [exclude: 0], 
-#            'star': [star: 0],
-#            'bowtie': [bowtie: 0], 
-#            'line': [line: 0], 
-#            'ribbon': [ribbon: 0]}
-
-# paths = ['/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/ys-by-yohji-yamamoto-regular-sneaker-off-white_product_9109337_color_527.xml']         
-for path in paths:
-    path=str(path)
-    tree = ET.parse(path)
-    root = tree.getroot()
-    for obj in root.findall('object'):
-        idx = 0
-        name = obj.find('name').text
-        # for cls in classes:
-        #     idx = name in cls
-        #     if idx:
-        #         count[str(cls[0])] += 1
-        # 
-        if name == 'elongated':
-            print(path)
-print('count')
-        
-count
-# {'logo': 1600, 'polygon': 55, 'octagon': 21, 'pentagon': 317, 'hexagon': 1133, 
-# 'chevron': 4479, 'circle': 5474, 'text': 7198, 'quad': 8987, 'other': 3543, 
-# 'triangle': 2784, 'exclude': 800, 'star': 1430, 'bowtie': 1175, 'line': 5214, 
-# 'ribbon': 256}
+    count
+    # {'logo': 1600, 'polygon': 55, 'octagon': 21, 'pentagon': 317, 'hexagon': 1133, 
+    # 'chevron': 4479, 'circle': 5474, 'text': 7198, 'quad': 8987, 'other': 3543, 
+    # 'triangle': 2784, 'exclude': 800, 'star': 1430, 'bowtie': 1175, 'line': 5214, 
+    # 'ribbon': 256}
---
+++ type_classes.py
@@ -0,0 +1,150 @@
+cm = ['qud', 'rectangle', 'hex', 'rounded', 'othre', 'hexagon', 'circle_', 'triangle',
+'curved', 'etxt', 'stars', 'qudarilateral', 'cirlce', 'quardilateral', 
+'quadrilateral', 'ext', 'cirle', 'quadrilaterl', 'chrevron', 'othere', 'star', 
+'triangl', 'elongated', 'chervron', 'toe', 'circles', 'texture_crepe', 
+'background', 'iother', 'hatching', 'qiad', 'lines', 'quadilateral', 
+'quadrilaterals', 'smooth', 'irregular', 'qua', 'ribbon', 'star.', 'circe', 
+'quadrliateral', 'ciricle', 'quad', 'octagon', 'texture', 'cricle', 'lkns', 
+'pentagons', 'crepe', 'texxt', 'chevrons', 'triangles', 'texture_smooth', 
+'bowite', 'blowtie', 'tet', 'oter', 'bowtie', 'circleline', 'pentagon', 
+'quarilateral', 'heagon', 'region', 'ttriangle', 'start', 'star,', 'triangels', 
+'circle', 'qaud', 'heel', 'cheron', 'quadrilateralline', 'chrvron', 'polygon', 
+'trianlges', 'lie', 'quadrilatteral', 'excludue', 'quadmline', 'quadrilteral', 
+'trianglee', 'exclud', 'other', 'quadrilaterlal', 'exclude', 'ribon', 'cicle', 
+'text', 'logo', 'qyad', 'chevron', 'line', 'bootie', 'quadcircle']
+
+
+
+
+logo = ['logo']
+
+polygon = ['polygon']
+
+octagon = ['octagon'] # 21
+
+pentagon = ['pentagon', 'pentagons']
+
+hexagon = ['hexagon', 'heagon', 'hex'] # 1133
+
+chevron = ['chevron', 'chrevron', 'chervron', 'chrvron', 'chevrons', 'cheron', ]
+
+circle = ['circle', 'circle_', 'cirlce', 'cirle', 'circles', 'circe', 'ciricle',
+          'cricle', 'cicle']
+          
+text = ['text', 'etxt', 'ext', 'tet', 'texxt']
+
+quad = ['quad', 'qud', 'qiad', 'qua', 'qyad', 'qaud',
+        'quadrilatteral', 'quadrliateral', 'quadrilaterals',
+        'quardilateral', 'quadrilaterlal','quarilateral', 
+        'quadrilaterl', 'quadilateral', 'qudarilateral', 'quadrilteral', 
+        'rectangle', 'quadrilateral']
+        
+other = ['other', 'othre', 'othere', 'iother', 'oter']
+
+triangle = ['triangle', 'triangl', 'triangles', 'triangels', 'trianglee',
+            'trianlges', 'ttriangle']
+            
+exclude = ['exclude', 'exclud', 'excludue']
+
+star = ['star', 'start', 'star.', 'star,','stars']
+
+bowtie = ['bowtie', 'bootie', 'blowtie', 'bowite']
+
+line = ['line', 'lie', 'lines']
+
+ribbon = ['ribbon', 'ribon']
+
+elongated = ['elongated']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/skechers-performance-go-walk-joy-soothe-burgundy_product_9111058_color_7.xml
+
+toe = ['toe']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/dan-post-thin-blue-line-sand-blue-leather_product_9089659_color_41715.xml
+
+curved = ['curved']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/sorel-kids-cub-toddler-little-kid-big-kid-surplus-green-olive_product_8699081_color_621306.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/sorel-kids-cub-toddler-little-kid-big-kid-surplus-green-olive_product_8699081_color_621306.xml
+
+rounded = ['rounded']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/bobs-from-skechers-bobs-squad-glam-le-taupe_product_9065662_color_11.xml
+
+quadcircle = ['quadcircle']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/skechers-easy-going-tan_product_9089402_color_20.xml
+
+quadmline = ['quadmline']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/skechers-performance-go-walk-evolution-ultra-rapids-black_product_9111097_color_3.xml
+
+circleline = ['circleline']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/merrell-all-out-blaze-sieve-brindle-butterscotch_product_9164043_color_533174.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/on-cloud-navy-white_product_8379589_color_513.xml
+
+quadrilateralline = ['quadrilateralline']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/jessica-simpson-carnivela-moss-brown-split-suede_product_8974852_color_669353.xml
+
+lkns = ['lkns']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/ugg-kids-obie-waterproof-toddler-little-kid-norse-green_product_8920281_color_703795.xml
+
+texture = ['texture']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/hunter-kids-original-chelsea-little-kid-big-kid-black_product_8577026_color_3.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/onex-ariel-n-white-elastic_product_9035240_color_30091.xml
+
+region = ['region']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/new-balance-classics-mz501-red_product_9042319_color_585.xml
+
+smooth = ['smooth']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/nina-deena-silver-dreamland_product_9107077_color_680591.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/steve-madden-rachel-platform-sandal-rose-gold_product_9026962_color_35560.xml
+
+irregular = ['irregular']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/dr-martens-redfin-white-hydro-leather-white-hydro-leather_product_8983105_color_730236.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/nike-tanjun-se-team-red-vast-grey-obsidian-navy_product_8709415_color_717098.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/spring-step-heroic-black_product_8618717_color_3.xml
+
+crepe = ['crepe']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/cienta-kids-shoes-76997-toddler-little-kid-big-kid-tiel_product_8108081_color_680257.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/cienta-kids-shoes-76997-toddler-little-kid-big-kid-tiel_product_8108081_color_680257.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/cienta-kids-shoes-76997-toddler-little-kid-big-kid-tiel_product_8108081_color_680257.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/not-rated-xara-cream_product_9042119_color_1892.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/taos-footwear-carousel-2-blue-embossed-suede_product_8831224_color_638910.xml
+
+texture_smooth = ['texture_smooth']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/pikolinos-tarifa-06j-0015-cuero_product_8672795_color_23562.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/tory-burch-brannan-studded-sandal-leccio_product_9125287_color_717736.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/tory-burch-color-block-slip-on-sneaker-perfect-ivory-perfect-black_product_9125293_color_778247.xml
+
+background = ['background']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/not-rated-aleksa-peach_product_9042129_color_4912.xml
+
+texture_crepe = ['texture_crepe']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/wolverine-i-90-epx-carbonmax-wheat_product_8984202_color_716.xml
+heel = ['heel']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/dan-post-thin-blue-line-sand-blue-leather_product_9089659_color_41715.xml
+hatching = ['hatching']
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/dingo-adobe-rose-light-brown-distresssed_product_7644821_color_249614.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/report-madella-black_product_9035067_color_3.xml
+# /Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation/roper-american-beauty-fringe-sanded-brown-leather-red-white-fringe_product_9096963_color_769188.xml
+
+# paths = get_files('/Users/huamuxin/Documents/fastai-test/Modified data/Modified Annotation', extensions=['.xml']) # fastai
+class_map = set([])
+classes = [logo, polygon, octagon, pentagon, hexagon, chevron,
+           circle, text, quad, other, triangle, exclude, star,
+           bowtie, line, ribbon]
+count = {'logo': 0, 'polygon': 0, 'octagon': 0, 'pentagon': 0, 'hexagon': 0, 
+         'chevron': 0, 'circle': 0, 'text': 0, 'quad': 0, 'other': 0, 
+         'triangle': 0, 'exclude': 0, 'star': 0, 'bowtie': 0, 'line': 0, 
+         'ribbon': 0}
+# count_1 = {'logo': [logo:0],
+#            'polygon': [polygon: 0],
+#            'octagon': [octagon: 0],
+#            'pentagon': [pentagon: 0], 
+#            'hexagon': [hexagon: 0], 
+#            'chevron': [chevron: 0], 
+#            'circle': [circle: 0], 
+#            'text': [text: 0], 
+#            'quad': [quad: 0], 
+#            'other': [other: 0], 
+#            'triangle': [triangle: 0], 
+#            'exclude': [exclude: 0], 
+#            'star': [star: 0],
+#            'bowtie': [bowtie: 0], 
+#            'line': [line: 0], 
+#            'ribbon': [ribbon: 0]}
